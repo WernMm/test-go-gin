a0ee423a884687fb38255109e0e597abdfd20e2b
Somwut <wern_sj@hotmail.com>
2018-12-24 16:26:30 +0700

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+docker build -t go-gin .
+docker build -t go-gin -f Dockerfile_DEV .
+docker run -t go-gin
+docker ps
+docker inspect {CONTAINER_ID}
+
---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	// "fmt"
+	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/qor/admin"
+	// "github.com/qor/qor"
+	"net/http"
+)
+
+// Create a GORM-backend model
+type User struct {
+	gorm.Model
+	Name string
+}
+
+// Create another GORM-backend model
+type Product struct {
+	gorm.Model
+	Name        string
+	Description string
+}
+
+func main() {
+	DB, _ := gorm.Open("sqlite3", "demo.db")
+	DB.AutoMigrate(&User{}, &Product{})
+
+	// Initalize
+	Admin := admin.New(&admin.AdminConfig{DB: DB})
+
+	// Allow to use Admin to manage User, Product
+	Admin.AddResource(&User{})
+	Admin.AddResource(&Product{})
+
+	// initalize an HTTP request multiplexer
+	mux := http.NewServeMux()
+	Admin.MountTo("/admin", mux)
+
+	router := gin.Default()
+	router.Any("/admin/*resources", gin.WrapH(mux))
+	router.Use(static.Serve("/", static.LocalFile("./views", true)))
+
+	api := router.Group("/api")
+	{
+		api.GET("/", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "pong",
+			})
+		})
+	}
+
+	// Start and run the server
+	router.Run(":3000")
+}
